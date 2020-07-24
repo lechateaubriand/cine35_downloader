f8e20613129a3e8cdd75be907506666a554c1088
pi3_etoile <pi3_etoile@orange.fr>
2020-07-24 14:35:17 +0200

    add file maker from backoffice api

--- sources/cine35_downloader_menu.py
+++ sources/cine35_downloader_menu.py
@@ -2,12 +2,43 @@
 from cursesmenu import *
 from cursesmenu.items import *
 from time import sleep
-import ba_dl_variables
+import requests
+import json
+
 import cine35_download
 import cine35_file_maker
 
+import logging, logging.config
+import ba_dl_variables
+logging.config.dictConfig(ba_dl_variables.LOGGING)
+from pprint import pprint
 
-def make_file():
+
+def make_file_from_api():
+    file = input('Chemin du fichier à créer [%s]: ' % ba_dl_variables.default_file)
+    file = file or ba_dl_variables.default_file
+
+    # get info from backoffice
+    date_debut = input('Date de début des séances (format YYYY-MM-DD): ')
+    response = requests.get('https://www.etoilecinema.fr/api/seances?date_debut=%s' % date_debut)
+    backoffice_movies = json.loads(response.text)
+    logging.info("Movies info received from backoffice: ")
+    logging.info(backoffice_movies)
+
+    f = open(file, 'w')
+
+    distinct_titles = set([ movie['film_titre'] for movie in backoffice_movies ])
+    for title in distinct_titles:
+        movie_backoffice_infos = [ movie for movie in backoffice_movies if movie['film_titre'] == title ]
+        cine35_file_maker.addTrailerFromApi(f, movie_backoffice_infos)
+
+    f.close()
+    print("'''''''''''''''''''''''''''''''''")
+    print("fin de la construction du fichier")
+    print("'''''''''''''''''''''''''''''''''")
+
+
+def make_file_from_menu():
     file = input('Chemin du fichier à créer [%s]: ' % ba_dl_variables.default_file)
     file = file or ba_dl_variables.default_file
 
@@ -16,13 +47,13 @@ def make_file():
         choice = input('Ajouter une bande-annonce o/n [o] ? ')
         choice = choice or 'o'
         if choice == 'o' or choice == 'O':
-            cine35_file_maker.addTrailer(f)
+            cine35_file_maker.addTrailerFromMenu(f)
         else:
             break
     f.close()
-    print("'''''''''''''''''''''''''")
+    print("'''''''''''''''''''''''''''''''''")
     print("fin de la construction du fichier")
-    print("'''''''''''''''''''''''''")
+    print("'''''''''''''''''''''''''''''''''")
     sleep(2)
     
 
@@ -44,10 +75,12 @@ def launch_downloader():
 menu = CursesMenu("Cine35 Downloader", "Choisissez l'une des options suivantes:")
 
 # menu items
-file_factory_item = FunctionItem("Construire le fichier de download", make_file)
+file_factory_from_api = FunctionItem("Construire le fichier de download depuis le back office", make_file_from_api)
+file_factory_item = FunctionItem("Construire le fichier de download avec le menu", make_file_from_menu)
 download_item = FunctionItem("Lancer le downloader",  launch_downloader)
 
 # Once we're done creating them, we just add the items to the menu
+menu.append_item(file_factory_from_api)
 menu.append_item(file_factory_item)
 menu.append_item(download_item)
 
--- sources/cine35_file_maker.py
+++ sources/cine35_file_maker.py
@@ -15,7 +15,47 @@ class ba_date(datetime.date):
         return weekday[self.weekday()] + ' ' + str(self.day) + ' ' + months[self.month]
 
 
-def addTrailer(file):
+def addTrailerFromApi(file, movie_info):
+    """
+    add a line in the file for a trailer
+    :@param f: file object in which we will write
+    :@param trailer_infos: a list of dict. Each dict is one of the backoffice entry for this movie.
+    """
+    title = movie_info[0]['film_titre']
+    ba_url = movie_info[0]['ba_youtube']
+    end_date = datetime.date(1984, 1, 1)
+    broadcast_dates = []
+
+
+    for info in movie_info:
+        # convert date
+        date_array = info['date'].split("/")
+        day, month, year = int(date_array[0]), int(date_array[1]), int(date_array[2])
+        converted_date = ba_date(year, month, day)
+
+        # convert hour
+        current_broadcast_date = datetime.date(year, month, day)
+        if (current_broadcast_date > end_date):
+            end_date = current_broadcast_date
+
+        # get hour and VOSTF if exists
+        hour_array = info['heure'].split(":")
+        hour = "h".join(hour_array)
+        seance_type = ''
+        if 'VOSTF' in info['types_seance']:
+            seance_type = ' (VOSTF)'
+
+        # final broadcast date string
+        broadcast_date = str(converted_date) + ' à ' + hour + seance_type
+        broadcast_dates.append(broadcast_date)
+
+    end_date = '%s/%s/%s' % (end_date.day, end_date.month, end_date.year)
+    trailer = cine35_download.BandeAnnonce(title, ba_url, end_date, broadcast_dates)
+
+    file.write(str(trailer) + "\n")
+
+
+def addTrailerFromMenu(file):
     """
     add a line in the file for a trailer
     :@param f: file object in which we will write
